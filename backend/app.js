5b4f1312b98d438b5160376ad4836b4955e7b396
vooit <wojciechpac@gmail.com>
2018-11-23 00:45:57 +0100

    post and get data from mongo

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
 const Post = require('./models/post');
-
 const app = express();
 
-mongoose.connect("mongodb+srv://vooit:sEcZONRNuZJk4g9D@heavenbase-bu5f3.mongodb.net/test?&w=1", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://vooit:Solid234@heavenbase-bu5f3.mongodb.net/test?&w=1", { useNewUrlParser: true });
 
 // mongoose.connect('mongodb://${user}:${pass}@${uri}/${db}?authMechanism=SCRAM-SHA-1')
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +30,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  post.save()
+  post.save();
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
@@ -41,22 +38,12 @@ app.post("/api/posts", (req, res, next) => {
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
-  });
+  Post.find().then(data => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: data
+    });
+  })
 });
 
 module.exports = app;
