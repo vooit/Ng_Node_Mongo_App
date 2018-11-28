e63392e3c212aecac955d55d24b8ea3b4dd1c6e2
vooit <wojciechpac@gmail.com>
2018-11-28 22:46:24 +0100

    loader service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,31 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {
   MatInputModule,
   MatCardModule,
   MatButtonModule,
   MatToolbarModule,
-  MatExpansionModule
+  MatExpansionModule,
+  // MatProgressSpinnerModule
 } from "@angular/material";
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
 import { HeaderComponent } from "./header/header.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
-import {AppRoutingModule} from "./app-routing.module";
+import { AppRoutingModule } from "./app-routing.module";
+import { LoaderComponent } from './loader/loader.component';
+import {LoaderInterceptorService} from './services/loader-interceptor.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -34,9 +38,16 @@ import {AppRoutingModule} from "./app-routing.module";
     MatButtonModule,
     MatToolbarModule,
     MatExpansionModule,
+    // MatProgressSpinnerModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptorService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
